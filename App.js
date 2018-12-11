e9730dadce455ed27782d49f7b785af018e9339b
Caroline Salib <carolinesalibc@gmail.com>
2018-12-11 08:33:46 -0200

    Add FlatList with post photos

--- App.js
+++ App.js
@@ -7,24 +7,31 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Dimensions, FlatList} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const width = Dimensions.get('screen').width;
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    const photos = [
+      {id: 1, userName: 'rafael'},
+      {id: 2, userName: 'alberto'},
+      {id: 3, userName: 'vitor'},
+    ];
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <FlatList style={{marginTop: 20}}
+        keyExtractor={item => String(item.id)}
+        data={photos}
+        renderItem={ ({item}) =>
+          <View>
+            <Text>{item.userName}</Text>
+            <Image source={require('./resources/img/send.png')}
+                style={{width: width, height: width}} />
+          </View>
+        }
+      />
     );
   }
 }
